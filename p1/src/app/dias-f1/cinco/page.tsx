d090c315fcaff3ec968fdc8c634e1b9b58f6bbf1
gusandrini <gustavosandrini92@gmail.com>
2024-10-13 00:16:11 -0300

    add conteudo

--- p1/src/app/dias-f1/cinco/page.tsx
+++ p1/src/app/dias-f1/cinco/page.tsx
@@ -6,6 +6,25 @@ export default function Cinco() {
 
             <Conteudo />
 
+            <div className="treino">
+                <h1>Posterior</h1>
+                <h2>Exercicios:</h2>
+                <p>Cadeira flexora</p>
+                <p>Levantamento terra romeno – 4 séries de 8-10 repetições</p>
+                <p>Stiff com halteres – 4 séries de 10 repetições</p>
+                <p>Elevação de quadril (Hip Thrust) – 4 séries de 12-15 repetições</p>
+                <p>Cadeira abdutora – 4 séries de 12-15 repetições</p>
+                <p>Mesa flexora – 4 séries de 12-15 repetições</p>
+            </div>
+
+            <div className="treino">
+                <h1>Panturilha</h1>
+                <h2>Exercicios:</h2>
+                <p>Máquina sentado</p>
+                <p>Em pé na barra guiada</p>
+                <p></p>
+            </div>
+
         </div>
     );
 }
